1a4eab373bb02ae6822ade551a62ce832ce3a089
robertojff <robertojff3333@gmail.com>
2023-12-16 19:40:18 -0300

    primeiro commit

---
+++ constantes.js
@@ -0,0 +1,11 @@
+const valorIngressoAdulto = 30;
+console.log(valorIngressoAdulto);
+
+const valorDaContaDeAgua = 75;
+console.log(valorDaContaDeAgua);
+
+const valorDaContaDeLuz = 100;
+console.log(valorDaContaDeLuz);
+
+const valorDaContaDaInternet = 55;
+console.log(valorDaContaDaInternet);
